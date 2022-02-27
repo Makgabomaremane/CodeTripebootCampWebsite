b60a8e3084fa29b803c62433424678e2947b1d63
makgabo <makgabomaremane@gmail.com>
2022-02-27 09:55:41 +0200

    first commit final

--- AboutMe.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('info').innerHTML = "Basic Information";
-document.getElementById('name').innerHTML = "Full name : Makgabo Maremene<br>"+
-                                            "Surname : Maremane<br>"+
-                                            "idntity number : 990812 0567 082<br>";
-                                           
-                                            
-                                          
